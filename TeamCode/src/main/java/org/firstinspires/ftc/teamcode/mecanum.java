d380d84d9981e60443a441df8067df0e0f2a3a7f
ashwinrao <ashwinrao16@gmail.com>
2023-06-24 13:19:48 -0500

    Initialize Project

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="mecanum", group="LinearOpmode")
+public class mecanum extends OpMode {
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    //    private MecanumDrive drive;
+    private MecanumDrive drive;
+    private Motor fL, fR, bL, bR;
+    private GamepadEx driverOp;
+
+
+
+    @Override
+    public void init() {
+        /* instantiate motors */
+
+        drive = new MecanumDrive(fL, fR, bL, bR);
+        driverOp = new GamepadEx(gamepad1);
+    }
+
+    @Override
+    public void loop() {
+        drive.driveRobotCentric(
+                driverOp.getLeftX(),
+                driverOp.getLeftY(),
+                driverOp.getRightY()
+        );
+    }
+}
