398d545819edf0b04571953477dff1a18ebcee98
isaacedelman <59181475+isaacedelman@users.noreply.github.com>
2023-06-26 11:53:33 -0500

    Added direction test to mecanum.java
    I commented out some code and added something that should set each motor to power mode and then run it at 0.5 power. This should let us see which motors need to be reversed. We will remove this test once we figure it out.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum.java
@@ -25,12 +25,18 @@ public class mecanum extends OpMode {
     public void init() {
         /* instantiate motors */
         fL = new Motor(hardwareMap, "frontLeft");
-
         fR = new Motor(hardwareMap, "frontRight");
         bL = new Motor(hardwareMap, "rearLeft");
         bR = new Motor(hardwareMap, "rearRight");
-        bL.setInverted(true);
-        bR.setInverted(true);
+
+        // setting motors to power mode for testing
+        fL.setRunMode(Motor.RunMode.RawPower);
+        fR.setRunMode(Motor.RunMode.RawPower);
+        bL.setRunMode(Motor.RunMode.RawPower);
+        bR.setRunMode(Motor.RunMode.RawPower);
+        
+        // bL.setInverted(true);
+        // bR.setInverted(true);
         drive = new MecanumDrive(fL, fR, bL, bR);
         driverOp = new GamepadEx(gamepad1);
 
@@ -38,16 +44,19 @@ public class mecanum extends OpMode {
 
     @Override
     public void loop() {
-        drive.driveRobotCentric(
-                0.5*driverOp.getLeftX(),
+        // drive.driveRobotCentric(
+        //         0.5*driverOp.getLeftX(),
 
-                0.5*driverOp.getLeftY(),
-                0.5*driverOp.getRightX()
-
-
-
-
-        );
+        //         0.5*driverOp.getLeftY(),
+        //         0.5*driverOp.getRightX()
+        // );
+        
+        // run all motors in same direction to see which ones to reverse
+        fL.set(0.5);
+        fR.set(0.5);
+        bL.set(0.5);
+        bR.set(0.5);
+        
         telemetry.addData("Left Stick Y", driverOp.getLeftY());
         telemetry.addData("Left Stick X", driverOp.getLeftX());
         telemetry.addData("Right Stick X", driverOp.getRightX());
