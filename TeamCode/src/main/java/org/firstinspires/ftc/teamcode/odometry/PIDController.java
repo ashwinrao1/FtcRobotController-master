6336991789a7446e42d6407e3acdb2ea7f18c25b
isaacedelman <59181475+isaacedelman@users.noreply.github.com>
2023-08-19 14:51:31 -0500

    Update PIDController.java
    Added PID code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/PIDController.java
@@ -1,4 +1,57 @@
 package org.firstinspires.ftc.teamcode.odometry;
 
 public class PIDController {
+
+    double kP;
+    double kI;
+    double kD;
+    double integralSum;
+    double lastError;
+    double target;
+    ElapsedTime timer;
+    boolean isFinished;
+
+    final double ERROR_TOLERANCE = 0.01;
+    
+    
+    public PIDController (double kP, double kI, double kD)
+    {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+        this.integralSum = 0;
+        this.lastError = 0;
+        this.target = 0;
+        this.timer = new ElapsedTime();
+        this.isFinished = false;
+    }
+
+    public void setTarget(double val)
+    {
+        this.target = val;
+    }
+
+    public boolean isFinished()
+    {
+        return ( Math.abs(lastError) < ERROR_TOLERANCE ); 
+    }
+
+    public double calculate (double currPos)
+    {
+        double error = target - currPos;
+
+        double derivative = (error - lastError) / timer.seconds();
+
+        integralSum += error * timer.seconds();
+
+        double output = (error * kP) + (integralSum * kI) + (derivative * kD);
+
+        lastError = error;
+
+        timer.reset();
+
+        return output;
+    }
+
+    
 }
